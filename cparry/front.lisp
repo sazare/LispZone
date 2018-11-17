13e638a91ab1bfea530d51acfb7f65d21ee1c1c5
sazare <song.of.sand@gmail.com>
2018-11-17 19:58:44 +0900

    fixed break at make-infs caddr was list.
    added win.lisp
    	modified:   front.lisp
    	modified:   pmem4.lisp
    	new file:   win.lisp
    	modified:   ../cparrydev.log

--- cparry/front.lisp
+++ cparry/front.lisp
@@ -3,23 +3,23 @@
 (defun test_pattern ()
   ;; too long
   (prog ()
-  (get_question)
-  (windowset 1 SSENT)
-  (window 1 t 'INPUT)
-  (window 2 t SSENT)
-  ;(push p nil)
-  ;(push p nil)
-  ;(push p nil)
-  (unless (equal nil LEARNING)
-    (car SSENT)
-    RIGHT
-    (cdr SSENT)
-    SSENT
-    (window 1 t 'RESPELLED)
-    (window 3 t (find_words SSENT))
-    (when (synonm LEARNING) (return))
+    (get_question)
+    (windowset 1 SSENT)
+    (window 1 t 'INPUT)
+    (window 2 t SSENT)
+    ;(push p nil)
+    ;(push p nil)
+    ;(push p nil)
+    (unless (equal nil LEARNING)
+      (car SSENT)
+      RIGHT
+      (cdr SSENT)
+      SSENT
+      (window 1 t 'RESPELLED)
+      (window 3 t (find_words SSENT))
+      (when (synonm LEARNING) (return))
+      )
     )
-  )
   )
     
     
